cd34d31a59f122608ca0af408f0adbd14ba768d5
Hippo <307649576@qq.com>
2020-07-11 00:45:10 +0800

    test

--- frontend/online-shop/src/api/api.js
+++ frontend/online-shop/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// let host = 'http://www.flexrope.cn'
-let localHost = 'http://127.0.0.1:8000'
+let host = 'http://www.flexrope.cn:8000'
+// let host = 'http://127.0.0.1:8000'
 
 //  获取商品类别信息
-export const queryGoods = params => { return axios.get(`${localHost}/api/goods/`, { params: params }) }
-export const queryCategorys = params => { return axios.get(`${localHost}/api/categorys/`, { params: params }) }
+export const queryGoods = params => { return axios.get(`${host}/api/goods/`, { params: params }) }
+export const queryCategorys = params => { return axios.get(`${host}/api/categorys/`, { params: params }) }
